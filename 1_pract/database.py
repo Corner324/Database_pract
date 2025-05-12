29124c652dc3110acb859410b18ff14dc48f76d7
Corner324 <mosenn@mail.ru>
2025-05-12 17:41:46 +0300

    Добавление alembic

--- 1_pract/database.py
+++ 1_pract/database.py
@@ -4,6 +4,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+print("DATABASE URL - ", DATABASE_URL)
 
 BaseModel = declarative_base()
 
